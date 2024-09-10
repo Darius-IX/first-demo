99e7e69c5e5cdfc09084775298999a6dfdc0bb7d
Darius-IX <89026567+Darius-IX@users.noreply.github.com>
2024-09-11 00:07:29 +0200

    added workflow.yml

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Darius-IX.github.io/",
+  base: "/first-demo/",
   plugins: [react()],
   server: {
     host: true,
